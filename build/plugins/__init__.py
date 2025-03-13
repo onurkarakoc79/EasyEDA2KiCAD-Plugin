44618d23b5ac0369be963e66eca76353b308671e
Onur Karakoç <onurkarakoc79@gmail.com>
2025-03-13 20:18:18 +0300

    a

--- build/plugins/__init__.py
+++
@@ -1,7 +0,0 @@
-from .easyeda2kicad_action import PluginInjector
-
-# Start the injector timer to check for schematic windows every 2 seconds.
-injector = PluginInjector()
-injector.Start(2000)  # 2000 milliseconds = 2 seconds
-
-print("✅ EasyEDA2KiCAD Plugin Auto Injector Started")
